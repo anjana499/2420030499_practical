4f9e1d597c5a0f484e2e6294c1ece5ecb540e6ab
anjana499 <anjanasakhamuri2006@gmail.com>
2025-08-02 09:41:29 +0530

    Add files via upload

---
+++ data types.java
@@ -0,0 +1,19 @@
+package java_project_1;
+
+public class my_project {
+	public static void main(String args[]) {
+	int a =2;
+	char b ='A';
+	double c=3.6;
+	float d=5.6f;
+	String s="hello this is cse";
+	//System.out.println(a);
+	//System.out.println(b);
+	//System.out.println(c);
+	//System.out.println(d);
+	//System.out.println(s);
+	//System.out.println(s+""+a+""+b+""+""+c+""+d);
+	System.out.printf("intiger %d%n character %c%nndouble%f%nstring %s",a,b,c,s);
+	}
+    
+}
